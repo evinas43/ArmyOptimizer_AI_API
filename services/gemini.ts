aedc4ff2ea175f5104fe3e8142ff579e6dd5d10c
evinas43 <evinas.dam@institutcampalans.net>
2026-04-21 16:10:10 +0200

    test: new structure gemini

--- services/gemini.ts
+++ services/gemini.ts
@@ -10,18 +10,52 @@ export const geminiService: AIService = {
   name: "Gemini",
 
   async chat(messages: ChatMessage[]) {
-    const prompt = [
-      context,
-      ...messages.map(
-        m => `${m.role.toUpperCase()}: ${m.content}`
-      )
-    ].join("\n\n");
 
     const response = await genAI.models.generateContent({
-      model: "gemini-3.1-pro-preview",
-      contents: prompt
+      model: "gemini-1.5-flash",
+      contents: [
+        {
+          role: "user",
+          parts: [{ text: context }]
+        },
+        ...messages.map(m => ({
+          role: m.role === "assistant" ? "model" : "user",
+          parts: [{ text: m.content }]
+        }))
+      ]
     });
 
     return response.text ?? "";
   }
 };
+
+
+
+
+// import { GoogleGenAI } from "@google/genai";
+// import type { AIService, ChatMessage } from "../types";
+// import { context } from "../lib/const";
+
+// const genAI = new GoogleGenAI({
+//   apiKey: process.env.GEMINI_API_KEY
+// });
+
+// export const geminiService: AIService = {
+//   name: "Gemini",
+
+//   async chat(messages: ChatMessage[]) {
+//     const prompt = [
+//       context,
+//       ...messages.map(
+//         m => `${m.role.toUpperCase()}: ${m.content}`
+//       )
+//     ].join("\n\n");
+
+//     const response = await genAI.models.generateContent({
+//       model: "gemini-1.5-flash",
+//       contents: prompt
+//     });
+
+//     return response.text ?? "";
+//   }
+// };
